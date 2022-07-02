31a1d5c4336eba6c3e3927a34183e1392f51d3a8
yusuf <yusufkaleemkhan@gmail.com>
2022-07-02 20:29:04 +0530

    header change

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>My Favorite Beer</h1>
+       <h1>My Favorite Beer 1</h1>
       </header>
     </div>
   );
