96c79b8837f35f2803dfaef7ce186e06e57a21b2
yusuf <yusufkaleemkhan@gmail.com>
2022-07-02 20:27:33 +0530

    header change

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders Favorite Beer header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Favorite Beer/i);
+  const linkElement = screen.getByText(/Favorite Beers/i);
   expect(linkElement).toBeInTheDocument();
 });
