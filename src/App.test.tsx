e7240d0b49075725566ffb7e13fd1e22ddc20cac
yusuf <yusufkaleemkhan@gmail.com>
2022-07-02 17:39:06 +0530

    load the project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Favorite Beer header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Favorite Beer/i);
   expect(linkElement).toBeInTheDocument();
 });
