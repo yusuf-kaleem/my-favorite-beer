920f22c9a0874aa4a9769f74cdc5e0a19ecc9a52
yusuf <yusufkaleemkhan@gmail.com>
2022-07-04 14:25:49 +0530

    ading alt

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <div className="header" >
          {/* Beer Logo */}
-        <img className="m10" src={"https://images.punkapi.com/v2/58.png"} height="60" alt="weather-logo" />
+        <img alt="logo" className="m10" src={"https://images.punkapi.com/v2/58.png"} height="60" alt="weather-logo" />
          {/* Main Heading */}
         <span> My Favorite Beer </span> 
          {/* Search Componenet */}
