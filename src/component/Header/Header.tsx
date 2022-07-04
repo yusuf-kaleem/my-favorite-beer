c6a17f99692c4b26bcc0be29465e66558674df43
yusuf <yusufkaleemkhan@gmail.com>
2022-07-04 23:45:36 +0530

    adding table

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -3,10 +3,14 @@ import Search from '../Search/Search'
 function Header() {
     return (
         <div className="header" >
-         {/* Beer Logo */}
-        <img className="m10" src={"https://images.punkapi.com/v2/58.png"} height="60" alt="beer-logo" />
-         {/* Main Heading */}
-        <span> My Favorite Beer </span> 
+         
+
+         {/*Title & Beer Logo */}
+         <div className='flex_row bold_font'>
+         <span style={{marginTop: "5px"}}> My Favorite Beer </span> 
+         <img className="m10 rotate" src={"https://images.punkapi.com/v2/89.png"} height="80" alt="beer-logo" />
+         </div>
+     
          {/* Search Componenet */}
          <Search></Search>
 
--- src/component/Table/Table.tsx
+++ src/component/Table/Table.tsx
@@ -52,7 +52,7 @@ const App: React.FC = () => {
         setSaveList(JSON.parse(data ||'{}'))
     })    
 
-return (<Table columns={columns} dataSource={saveList} />)
+return (<Table  columns={columns} dataSource={saveList} />)
 };
 
 
