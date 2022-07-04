28111523f5a68f4cca833111249f00adbc3988f0
yusuf <yusufkaleemkhan@gmail.com>
2022-07-04 12:08:06 +0530

    adding header

---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+function Header() {
+    return (
+        <div className="header" >
+
+        <img className="m10" src={"https://images.punkapi.com/v2/58.png"} height="60" alt="weather-logo" />
+   
+         {/* Main Heading */}
+        <span> My Favorite Beer </span> 
+      </div>
+        );
+  }
+  
+  export default Header;
+  
