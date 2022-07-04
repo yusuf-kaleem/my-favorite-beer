b56614ce2e692049680f9aef02d3aa767cbcfd57
yusuf <yusufkaleemkhan@gmail.com>
2022-07-04 13:45:55 +0530

    adding search

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,12 +1,15 @@
+import Search from '../Search/Search'
 
 function Header() {
     return (
         <div className="header" >
-
+         {/* Beer Logo */}
         <img className="m10" src={"https://images.punkapi.com/v2/58.png"} height="60" alt="weather-logo" />
-   
          {/* Main Heading */}
         <span> My Favorite Beer </span> 
+         {/* Search Componenet */}
+         <Search></Search>
+
       </div>
         );
   }
